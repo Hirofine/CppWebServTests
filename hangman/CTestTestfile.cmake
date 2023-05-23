4f547566f9bc9bb68297afda2797b830f5b62eff
Hirofine <43345721+Hirofine@users.noreply.github.com>
2023-05-23 12:14:02 +0200

    thingys

---
+++ hangman/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hirofine/Documents/CppWebServTests/hangman
+# Build directory: /home/hirofine/Documents/CppWebServTests/hangman
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
